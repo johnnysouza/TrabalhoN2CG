ebef45a6197dd19abafa7412bb515992cd6c9ea1
Jonathan <johnnysouza3@gmail.com>
2015-03-21 11:44:34 -0300

    Exer1

--- TrabalhoN2CG/src/cg/exer1/Main.java
+++ TrabalhoN2CG/src/cg/exer1/Main.java
@@ -9,9 +9,11 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
+import cg.util.CircleUtils;
+
 public class Main implements GLEventListener, KeyListener {
-	
-	private float ortho2D_minX = -400.0f, ortho2D_maxX =  400.0f, ortho2D_minY = -400.0f, ortho2D_maxY =  400.0f;
+
+	private float ortho2D_minX = -400.0f, ortho2D_maxX = 400.0f, ortho2D_minY = -400.0f, ortho2D_maxY = 400.0f;
 	private GL gl;
 	private GLU glu;
 	private GLAutoDrawable glDrawable;
@@ -23,29 +25,40 @@ public class Main implements GLEventListener, KeyListener {
 		glu = new GLU();
 		glDrawable.setGL(new DebugGL(gl));
 		System.out.println("Espaco de desenho com tamanho: " + drawable.getWidth() + " x " + drawable.getHeight());
-		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		gl.glClearColor(0.85f, 0.85f, 0.85f, 1.0f); //Cinza claro de fundo
 	}
-	
-	//exibicaoPrincipal
-	public void display(GLAutoDrawable arg0) {
-		 gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-		 gl.glMatrixMode(GL.GL_PROJECTION);
-		 gl.glLoadIdentity();
-		 glu.gluOrtho2D( ortho2D_minX,  ortho2D_maxX,  ortho2D_minY,  ortho2D_maxY);
-		 gl.glMatrixMode(GL.GL_MODELVIEW);
-		 gl.glLoadIdentity();
 
-		 SRU();
-		 
-		 // seu desenho ...
-		 //TODO
-	}	
+	public void display(GLAutoDrawable arg0) {
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		gl.glMatrixMode(GL.GL_PROJECTION);
+		gl.glLoadIdentity();
+		glu.gluOrtho2D(ortho2D_minX, ortho2D_maxX, ortho2D_minY, ortho2D_maxY);
+		gl.glMatrixMode(GL.GL_MODELVIEW);
+		gl.glLoadIdentity();
+
+		SRU();
+
+		// seu desenho ...
+		gl.glColor3f(0.0f, 0.0f, 1.0f);
+		gl.glPointSize(2.0f);
+
+		double x = 0;
+		double y = 0;
+		gl.glBegin(GL.GL_POINTS);
+		for (int ang = 0; ang < 360; ang = ang + 5) {
+			x = CircleUtils.RetornaX(ang, 100);
+			y = CircleUtils.RetornaY(ang, 100);
+			gl.glVertex2d(x, y);
+			gl.glVertex2d(x, y);
+		}
+		gl.glEnd();
+	}
 
 	public void keyPressed(KeyEvent e) {
 		System.out.println(" --- keyPressed ---");
-		
+
 		System.out.println(" --- Redesenha ao sair do callback ---");
-		glDrawable.display();  // redesenhar ...
+		glDrawable.display(); // redesenhar ...
 	}
 
 	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
@@ -63,25 +76,25 @@ public class Main implements GLEventListener, KeyListener {
 	public void keyTyped(KeyEvent arg0) {
 		System.out.println(" --- keyTyped ---");
 	}
-	
+
 	public void SRU() {
-//		gl.glDisable(gl.GL_TEXTURE_2D);
-//		gl.glDisableClientState(gl.GL_TEXTURE_COORD_ARRAY);
-//		gl.glDisable(gl.GL_LIGHTING);
+		// gl.glDisable(gl.GL_TEXTURE_2D);
+		// gl.glDisableClientState(gl.GL_TEXTURE_COORD_ARRAY);
+		// gl.glDisable(gl.GL_LIGHTING);
 
 		// eixo x
 		gl.glLineWidth(1.0f);
 
 		gl.glColor3f(1.0f, 0.0f, 0.0f);
-		gl.glBegin( GL.GL_LINES );
-			gl.glVertex2f( -200.0f, 0.0f );
-			gl.glVertex2f(  200.0f, 0.0f );
-			gl.glEnd();
+		gl.glBegin(GL.GL_LINES);
+		gl.glVertex2f(-200.0f, 0.0f);
+		gl.glVertex2f(200.0f, 0.0f);
+		gl.glEnd();
 		// eixo y
 		gl.glColor3f(0.0f, 1.0f, 0.0f);
-		gl.glBegin( GL.GL_LINES);
-			gl.glVertex2f(  0.0f, -200.0f);
-			gl.glVertex2f(  0.0f, 200.0f );
+		gl.glBegin(GL.GL_LINES);
+		gl.glVertex2f(0.0f, -200.0f);
+		gl.glVertex2f(0.0f, 200.0f);
 		gl.glEnd();
 	}
 
